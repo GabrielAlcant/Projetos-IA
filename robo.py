515134017422c1e88767c5fbdc7b05f38102bc8b
Gabriel Ribeiro <77732873+GabrielAlcant@users.noreply.github.com>
2023-06-23 12:06:30 -0300

    Update and rename Robo.py to robo.py

--- robo.py
+++ robo.py
@@ -15,30 +15,32 @@ class Room:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.dirty = np.random.choice([True, False])
-        self.color = (255, 255, 255) if self.dirty else (200, 200, 200)
+        self.dirty = np.random.choice([True, False])  # Define aleatoriamente se o quarto está sujo ou não
+        self.color = (255, 255, 255) if self.dirty else (200, 200, 200)  # Define a cor do quarto com base no estado de sujeira
 
     def draw(self, screen):
-        rect = pygame.Rect(self.x * CELL_SIZE, self.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-        pygame.draw.rect(screen, self.color, rect)
+        rect = pygame.Rect(self.x * CELL_SIZE, self.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)  # Cria um retângulo para representar o quarto
+        pygame.draw.rect(screen, self.color, rect)  # Desenha o retângulo na tela com a cor apropriada
 
 # Defina uma classe Robot com um método de inicialização que define as coordenadas, raio e cor do robô. Ele também possui um método de desenho para desenhar o robô na tela e os métodos sense e clean para detectar e limpar quartos sujos.
 class Robot:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.radius = CELL_SIZE // 2
-        self.color = (255, 0, 0)
+        self.radius = CELL_SIZE // 2  # Define o raio do robô com base no tamanho da célula
+        self.color = (255, 0, 0)  # Define a cor do robô como vermelho
+        self.cleaned_rooms = 0  # Contador para quartos limpos pelo robô
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.x * CELL_SIZE + self.radius, self.y * CELL_SIZE + self.radius), self.radius)
+        pygame.draw.circle(screen, self.color, (self.x * CELL_SIZE + self.radius, self.y * CELL_SIZE + self.radius), self.radius)  # Desenha o robô como um círculo na tela
 
     def sense(self, rooms):
-        return rooms[self.x][self.y].dirty
+        return rooms[self.x][self.y].dirty  # Retorna o estado de sujeira do quarto atual em que o robô está
 
     def clean(self, rooms):
-        rooms[self.x][self.y].dirty = False
-        rooms[self.x][self.y].color = (200, 200, 200)
+        rooms[self.x][self.y].dirty = False  # Marca o quarto atual como limpo
+        rooms[self.x][self.y].color = (200, 200, 200)  # Atualiza a cor do quarto para refletir seu estado limpo
+        self.cleaned_rooms += 1  # Incrementa o contador de quartos limpos
 
 # Defina uma função para gerar uma lista de quartos com base na constante NUM_ROOMS. Ele cria uma instância Room para cada célula da grade e os adiciona a cada linha antes de adicionar a linha à lista final.
 def generate_rooms():
@@ -46,44 +48,44 @@ def generate_rooms():
     for i in range(NUM_ROOMS):
         row = []
         for j in range(NUM_ROOMS):
-            room = Room(i, j)
-            row.append(room)
-        rooms.append(row)
+            room = Room(i, j)  # Cria uma instância de Room com as coordenadas atuais
+            row.append(room)  # Adiciona o quarto à linha atual
+        rooms.append(row)  # Adiciona a linha à lista de quartos
     return rooms
 
 # Defina uma função de desenho que preenche a tela com a cor de fundo, chama o método de desenho para cada quarto e chama o método de desenho para o robô.
 def draw(screen, rooms, robot):
-    screen.fill(BG_COLOR)
+    screen.fill(BG_COLOR)  # Preenche a tela com a cor de fundo
     for row in rooms:
         for room in row:
-            room.draw(screen)
-    robot.draw(screen)
-    pygame.display.update()
+            room.draw(screen)  # Desenha cada quarto na tela
+    robot.draw(screen)  # Desenha o robô na tela
+    pygame.display.update()  # Atualiza a tela para mostrar as mudanças
 
 # Defina uma função de busca em profundidade que receba a lista de quartos e a instância do robô. Ela inicializa um conjunto vazio para os quartos visitados e uma lista para manter o registro do caminho do robô. Em seguida, retira as coordenadas do final do caminho e as adiciona ao conjunto de quartos visitados. Define as coordenadas do robô para a coordenada retirada e verifica se o quarto está sujo. Se estiver, o robô o limpa. Em seguida, obtém os quartos vizinhos e os adiciona à pilha se ainda não tiverem sido visitados. A função também chama a função "draw" para atualizar a tela a cada novo movimento.
 def depth_first_search(rooms, robot):
-    visited = set()
-    stack = [(robot.x, robot.y)]
+    visited = set()  # Conjunto de quartos visitados
+    stack = [(robot.x, robot.y)]  # Pilha para rastrear o caminho do robô
 
     while stack:
-        x, y = stack.pop()
-        visited.add((x, y))
+        x, y = stack.pop()  # Remove as coordenadas do final do caminho
+        visited.add((x, y))  # Adiciona as coordenadas aos quartos visitados
 
-        robot.x = x
+        robot.x = x  # Define as coordenadas do robô para a coordenada retirada
         robot.y = y
 
-        if robot.sense(rooms):
-            robot.clean(rooms)
+        if robot.sense(rooms):  # Verifica se o quarto atual está sujo
+            robot.clean(rooms)  # Limpa o quarto, se estiver sujo
 
-        neighbors = get_neighbors(x, y, rooms)
+        neighbors = get_neighbors(x, y, rooms)  # Obtém os quartos vizinhos
         for neighbor in neighbors:
-            if neighbor not in visited:
+            if neighbor not in visited:  # Adiciona vizinhos não visitados à pilha
                 stack.append(neighbor)
         
-        draw(screen, rooms, robot)
-        pygame.time.delay(200)
+        draw(screen, rooms, robot)  # Atualiza a tela para mostrar o movimento do robô
+        pygame.time.delay(200)  # Pausa para uma melhor visualização dos movimentos
 
-    robot.color = (0, 255, 0)
+    robot.color = (0, 255, 0)  # Define a cor do robô para verde após a conclusão
     draw(screen, rooms, robot)
     pygame.time.delay(1000)
 
@@ -102,9 +104,9 @@ def get_neighbors(x, y, rooms):
 
 # Define a função principal que cria a lista de quartos e a instância do robô. Em seguida, ela chama a função de busca em profundidade com essas entradas.
 def main():
-    rooms = generate_rooms()
-    robot = Robot(0, 0)
-    depth_first_search(rooms, robot)
+    rooms = generate_rooms()  # Gera a lista de quartos
+    robot = Robot(np.random.randint(NUM_ROOMS), np.random.randint(NUM_ROOMS))  # Cria uma instância de Robot com coordenadas aleatórias
+    depth_first_search(rooms, robot)  # Executa a busca em profundidade
 
 # Inicialize o Pygame e defina o tamanho da tela e a legenda. Em seguida, chame a função principal e encerre o Pygame.
 if __name__ == '__main__':
